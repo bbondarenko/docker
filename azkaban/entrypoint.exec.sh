eda09e993ae692e0eb606aaade54f56c890968c8
Boris Bondarenko <borisbond@gmail.com>
2023-02-16 20:46:18 +0000

    chore(container): Revamped container ggeneration using env

---
+++ azkaban/entrypoint.exec.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+export SPARK_HOME="/usr/local/spark"
+export PATH="$PATH:${SPARK_HOME}/bin:${SPARK_HOME}/sbin"
+export SPARK_DIST_CLASSPATH=$(hadoop classpath)
+
+export HADOOP_USER_NAME="hadoop"
+export HADOOP_HOME="/usr/local//hadoop"
+export HADOOP_CONF_DIR="${HADOOP_HOME}/etc/hadoop"
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${HADOOP_HOME}/lib/native"
+export JAVA_HOME="{{ fact_java_home }}"
+export PATH="${PATH}:${HADOOP_HOME}/bin:${HADOOP_HOME}/sbin"
+
+
+cd  /opt/azkaban
+/opt/azkaban/bin/internal/internal-start-executor.sh "$@"
